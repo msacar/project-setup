8a3590ca097c9ece31ec5b0a8a60b2ed81a5ac92
Mustafa Acar <mstkmstkk@hotmail.com>
2024-08-28 10:20:50 +0300

    projects_setup now can be run with direct urls

--- projects_setup.groovy
+++ projects_setup.groovy
@@ -5,6 +5,31 @@ def yaml = new Yaml()
 def projects = yaml.load(projectsConfig)
 
 
+
+import org.yaml.snakeyaml.Yaml
+import jenkins.model.Jenkins
+import java.util.zip.ZipInputStream
+import hudson.FilePath
+
+def projectsConfig = new File('/bitnami/jenkins/projects-config/projects-config.yaml').text
+def yaml = new Yaml()
+def projects = yaml.load(projectsConfig)
+
+def downloadAndExtractZip(url, destDir) {
+    new URL(url).openStream().withStream { is ->
+        def zis = new ZipInputStream(is)
+        def entry
+        while (entry = zis.nextEntry) {
+            if (!entry.isDirectory()) {
+                new File(destDir, entry.name).parentFile?.mkdirs()
+                new File(destDir, entry.name).withOutputStream { os ->
+                    os << zis
+                }
+            }
+        }
+    }
+}
+
 projects.projects.each { projectName, projectConfig ->
     folder(projectName) {
         description("Folder for ${projectName}")
@@ -13,16 +38,29 @@ projects.projects.each { projectName, projectConfig ->
     projectConfig.jobs.each { jobName, jenkinsfile ->
         pipelineJob("${projectName}/${jobName}") {
             definition {
-                cpsScm {
-                    scm {
-                        git {
-                            remote {
-                                url(projectConfig.gitUrl)
+                if (projectConfig.gitUrl) {
+                    cpsScm {
+                        scm {
+                            git {
+                                remote {
+                                    url(projectConfig.gitUrl)
+                                }
+                                branch(projectConfig.branch)
                             }
-                            branch(projectConfig.branch)
                         }
+                        scriptPath(jenkinsfile)
                     }
-                    scriptPath(jenkinsfile)
+                } else if (projectConfig.directUrl) {
+                    cps {
+                        def tempDir = new FilePath(new File("${Jenkins.instance.rootDir}/workspace/${projectName}-${jobName}-temp"))
+                        tempDir.deleteRecursive()
+                        tempDir.mkdirs()
+
+                        downloadAndExtractZip(projectConfig.directUrl, tempDir.remote)
+
+                        def jenkinsfilePath = new FilePath(tempDir, jenkinsfile)
+                        script(jenkinsfilePath.readToString())
+                    }
                 }
             }
         }
