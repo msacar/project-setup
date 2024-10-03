0ea596cf891ed9853d9bf16d248a44f510bea572
Mustafa Acar <mstkmstkk@hotmail.com>
2024-10-03 10:34:53 +0300

    Data Retrieval job

--- projects.ts
+++ projects.ts
@@ -1,15 +1,15 @@
 const yaml = require('js-yaml')
 const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.MONGO_CONNECTION_URI;
-const client = new MongoClient(uri);
+const { MONGO_CONNECTION_URI } = process.env;
+
+console.log(MONGO_CONNECTION_URI)
+const client = new MongoClient(MONGO_CONNECTION_URI);
+
 client.connect().then(() => console.log('Connected!')).catch(console.error);
-
 async function main() {
-
     const collection = client.db("root").collection("Projects");
     const projects = await collection.find()
-
-
+    console.log(projects)
 }
 
 main()
