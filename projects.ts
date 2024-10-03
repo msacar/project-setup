201aa8d1a162beae454f508a9019404b4f0f014d
Mustafa Acar <mstkmstkk@hotmail.com>
2024-10-03 10:38:20 +0300

    Data Retrieval job

--- projects.ts
+++ projects.ts
@@ -8,7 +8,7 @@ const client = new MongoClient(MONGO_CONNECTION_STRING);
 client.connect().then(() => console.log('Connected!')).catch(console.error);
 async function main() {
     const collection = client.db("root").collection("Projects");
-    const projects = await collection.find()
+    const projects = await collection.find().toArray()
     console.log(projects)
 }
 
