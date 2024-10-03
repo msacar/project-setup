9465716ee22a705fb27ed1756cc89d6a8a0dbb13
Mustafa Acar <mstkmstkk@hotmail.com>
2024-10-03 11:07:18 +0300

    Data Retrieval job

--- projects.ts
+++ projects.ts
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const yaml = require('js-yaml')
 const MongoClient = require('mongodb').MongoClient;
 const { MONGO_CONNECTION_STRING } = process.env;
@@ -6,11 +7,29 @@ console.log(MONGO_CONNECTION_STRING)
 const client = new MongoClient(MONGO_CONNECTION_STRING);
 
 client.connect().then(() => console.log('Connected!')).catch(console.error);
+
 async function main() {
     const collection = client.db("root").collection("Project");
-    const projects = await collection.find().toArray()
+    const projectsData = await collection.find().toArray()
+    const projects =  projectsData.map(p => {
+        return {
+            [p._rio_pk]: {
+                gitUrl: "git@github.com:rettersoft/rio-kubernetes-jenkinsfiles.git",
+                branch: "main",
+                jobs: {
+                    build: "user-code-template/build.Jenkinsfile",
+                    deploy: "user-code-template/deploy.Jenkinsfile"
+                }
+            }
+        }
+    })
+    fs.writeFileSync("project-config.yaml",yaml.dump({projects}))
+    console.log({yaml : yaml.dump({projects})})
     console.log(projects)
     process.exit(0)
 }
 
-main()
+main().catch(err=>{
+    console.error(err)
+    process.exit(1)
+});
